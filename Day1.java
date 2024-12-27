631fb52239599252a0b3e283dc4f83bb038e2325
Zay Yar Phyo <streammineking@gmail.com>
2024-12-27 17:12:21 +0630

    Dec 23

---
+++ Day1.java
@@ -0,0 +1,45 @@
+class Main {
+    public static void main(String [] args){
+    
+        String name = "Maung Maung";
+
+        /**
+         * DataType int, String 
+         * variable name (name, x)
+         * assigement operator = 
+         * value ("---", 10)
+         * end of statement ; 
+         */
+
+       System.out.println("Hello " + name);
+       System.out.println("Hello " + name);
+       System.out.println("Hello " + name);
+       System.out.println("Hello " + name);
+       System.out.println("Hello " + name);
+       System.out.println("Hello " + name);
+       System.out.println("Hello " + name);
+       System.out.println("Hello " + name);
+
+       System.out.println(10);
+       System.out.println('a');
+       System.out.println(true);
+       System.out.println(23.45);
+
+       boolean bol = false;
+       System.out.println(bol);
+
+       char ch = 'ခ';
+
+       byte b = 10;
+       byte b2 = 20;
+       short result = (byte)(b + b2);
+
+       long a = 12345678901L;
+
+       System.out.println(a);
+       
+    }
+}
+
+
+
